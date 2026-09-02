015951b437919cf69326247b885a8dce101ba234
JCS93 <192689701+j12Ch05@users.noreply.github.com>
2026-09-02 20:29:32 +0300

    EndlessActivity: fixing the buttons and stopping the loop

--- app/src/main/java/com/example/guessthecountry/EndlessActivity.kt
+++ app/src/main/java/com/example/guessthecountry/EndlessActivity.kt
@@ -1,15 +1,26 @@
 package com.example.guessthecountry
 
+import android.content.Intent
+import android.content.res.ColorStateList
+import android.graphics.Color
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
 import com.example.guessthecountry.databinding.ActivityEndlessBinding
 import org.json.JSONArray
 import java.lang.reflect.Field
+import androidx.lifecycle.lifecycleScope
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
 class EndlessActivity(): AppCompatActivity() {
     private lateinit var binding: ActivityEndlessBinding
     private val countries: List<Map<String, String>> by lazy { gettingData() }
-    lateinit var answer: Map<String,String>
+    private val used: ArrayList<String?> = arrayListOf()
+    private lateinit var answer: Map<String,String>
+    private var buttonNumber: Int = 0
+    private var currScore:Int = 0
+
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -17,8 +28,51 @@ class EndlessActivity(): AppCompatActivity() {
         binding = ActivityEndlessBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        loadQuestion()
+        val buttons = listOf(binding.btAns1, binding.btAns2, binding.btAns3, binding.btAns4,)
+
+        for (button in buttons) {
+            button.setOnClickListener {
+                val selectedAnswer = button.text.toString()
+                val correctAnswer = answer["country"]
+
+                if (selectedAnswer == correctAnswer) {
+                    currScore++
+                    if (currScore > countries.size){
+                        val intent: Intent = Intent(this, MainActivity::class.java)
+                        startActivity(intent)
+                        finish()
+                    }
+
+                    if ()
+                    binding.currentScore.text = currScore.toString()
+                    loadQuestion()
+                } else {
+                    val greenColor = Color.parseColor("#F54927")
+                    button.backgroundTintList = ColorStateList.valueOf(greenColor)
+                    lifecycleScope.launch {
+
+                        delay(500)
+
+
+                        val intent = Intent(this@EndlessActivity, MainActivity::class.java)
+                        startActivity(intent)
+
+
+                        finish()
+                    }
+                }
+            }
+        }
+
+    }
+
+
+
+    private fun loadQuestion(){
         if(countries.isNotEmpty()){
-            answer= countries.random()
+            answer= countries.filterNot{ it["country"] in used}.random()
+            used.add(answer["country"])
             val imageName = answer["id"]?.lowercase() ?: ""
 
             val imageRes = resources.getIdentifier(imageName,"drawable",packageName)
@@ -27,11 +81,51 @@ class EndlessActivity(): AppCompatActivity() {
                 binding.flagImage.setImageResource(imageRes)
             }
 
-
+            buttonNumber = (1..4).random()
         }
 
+        if(buttonNumber !=0){
+            val used1:ArrayList<String> = arrayListOf()
+            when(buttonNumber){
+                1 -> {
+                    binding.btAns1.text = answer["country"] ?: ""
+                    used1.add(binding.btAns1.text.toString())
+                    binding.btAns2.text = countries.filter{it["difficulty"] == answer["difficulty"]}.filterNot{it["country"] in used1}.random()["country"] ?: ""
+                    used1.add(binding.btAns2.text.toString())
+                    binding.btAns3.text = countries.filter{it["difficulty"] == answer["difficulty"] }.filterNot { it["country"] in used1 }.random()["country"] ?: ""
+                    used1.add(binding.btAns3.text.toString())
+                    binding.btAns4.text = countries.filter{it["difficulty"] == answer["difficulty"]}.filterNot { it["country"] in used1 }.random()["country"] ?: ""
+                }
+                2 -> {
+                    binding.btAns2.text = answer["country"] ?: ""
+                    used1.add(binding.btAns2.text.toString())
+                    binding.btAns1.text = countries.filter{it["difficulty"] == answer["difficulty"]}.filterNot{it["country"] in used1}.random()["country"] ?: ""
+                    used1.add(binding.btAns1.text.toString())
+                    binding.btAns3.text = countries.filter{it["difficulty"] == answer["difficulty"] }.filterNot { it["country"] in used1 }.random()["country"] ?: ""
+                    used1.add(binding.btAns3.text.toString())
+                    binding.btAns4.text = countries.filter{it["difficulty"] == answer["difficulty"]}.filterNot { it["country"] in used1 }.random()["country"] ?: ""
+                }
+                3 -> {
+                    binding.btAns3.text = answer["country"] ?: ""
+                    used1.add(binding.btAns3.text.toString())
+                    binding.btAns1.text = countries.filter{it["difficulty"] == answer["difficulty"]}.filterNot{it["country"] in used1}.random()["country"] ?: ""
+                    used1.add(binding.btAns1.text.toString())
+                    binding.btAns2.text = countries.filter{it["difficulty"] == answer["difficulty"] }.filterNot { it["country"] in used1 }.random()["country"] ?: ""
+                    used1.add(binding.btAns2.text.toString())
+                    binding.btAns4.text = countries.filter{it["difficulty"] == answer["difficulty"]}.filterNot { it["country"] in used1 }.random()["country"] ?: ""
+                }
+                4 -> {
+                    binding.btAns4.text = answer["country"] ?: ""
+                    used1.add(binding.btAns4.text.toString())
+                    binding.btAns1.text = countries.filter{it["difficulty"] == answer["difficulty"]}.filterNot{it["country"] in used1}.random()["country"] ?: ""
+                    used1.add(binding.btAns1.text.toString())
+                    binding.btAns2.text = countries.filter{it["difficulty"] == answer["difficulty"] }.filterNot { it["country"] in used1 }.random()["country"] ?: ""
+                    used1.add(binding.btAns2.text.toString())
+                    binding.btAns3.text = countries.filter{it["difficulty"] == answer["difficulty"]}.filterNot { it["country"] in used1 }.random()["country"] ?: ""
+                }
 
-
+            }
+        }
     }
 
     private fun gettingData(): List<Map<String,String>>{
@@ -49,6 +143,7 @@ class EndlessActivity(): AppCompatActivity() {
                 "country" to countryObject.getString("country"),
                 "capital" to countryObject.getString("capital"),
                 "region" to countryObject.getString("region"),
+                "difficulty" to countryObject.getString("difficulty")
             )
 
             dataset.add(m1)
